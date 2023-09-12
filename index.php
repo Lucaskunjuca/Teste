d2434752aa860e38082170e783455e1d7634bd58
Lucaskunjuca <kenylucaskananayfilipekunjuca@gmail.com>
2023-09-12 09:11:15 +0100

    :tada: initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+     
+     echo "Olá seja bem vindo as aulas de php";
+    $nome = "Lucas Kunjuca";
+    $idade = 18;
+
+    $a = 23;
+    $b = 45;
+
+    $soma = $a + $b;
+    
+    echo ("<br> O vaor da soma é $soma");
+
+    echo " <br>  Eu sou o $nome e tenho $idade anos de idade";
+
+    
+
+
+?>
